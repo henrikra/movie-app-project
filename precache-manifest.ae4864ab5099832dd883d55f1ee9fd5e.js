1079e682aa4df75bf5a5120a1f33026a7bf12296
henrikra <henrik.raitasola@gmail.com>
2019-02-02 12:56:05 +0000

    Updates

--- precache-manifest.ae4864ab5099832dd883d55f1ee9fd5e.js
+++ precache-manifest.ae4864ab5099832dd883d55f1ee9fd5e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/movie-app-project/static/js/runtime~main.ac3a6d10.js"
   },
   {
-    "revision": "a6bdd88ac9dbd0e95b4a",
-    "url": "/movie-app-project/static/js/main.a6bdd88a.chunk.js"
+    "revision": "37a83f196c1f8659b9cb",
+    "url": "/movie-app-project/static/js/main.37a83f19.chunk.js"
   },
   {
     "revision": "71e3705946188729e691",
     "url": "/movie-app-project/static/js/1.71e37059.chunk.js"
   },
   {
-    "revision": "a6bdd88ac9dbd0e95b4a",
-    "url": "/movie-app-project/static/css/main.bf655973.chunk.css"
+    "revision": "37a83f196c1f8659b9cb",
+    "url": "/movie-app-project/static/css/main.5572e5da.chunk.css"
   },
   {
-    "revision": "781d1073b45af153e064e2e46685efe7",
+    "revision": "c4e0c2ecea0c7aacd862e58d95ab13ee",
     "url": "/movie-app-project/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-app-project/precache-manifest.29d17eda0a6fe376c26ac1a2b3329588.js"
+  "/movie-app-project/precache-manifest.ae4864ab5099832dd883d55f1ee9fd5e.js"
 );
 
 workbox.clientsClaim();
