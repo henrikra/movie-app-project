c0895ed1b1ab1d56b854d48fc00f1cc44f1a79cd
henrikra <henrik.raitasola@gmail.com>
2019-02-02 12:14:57 +0000

    Updates

--- precache-manifest.c943a123254b2821a4bb42b87aaa09ba.js
+++ precache-manifest.c943a123254b2821a4bb42b87aaa09ba.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/movie-app-project/static/js/runtime~main.ac3a6d10.js"
   },
   {
-    "revision": "adc695b2d30ed970c531",
-    "url": "/movie-app-project/static/js/main.adc695b2.chunk.js"
+    "revision": "2a06c553ef2318014dec",
+    "url": "/movie-app-project/static/js/main.2a06c553.chunk.js"
   },
   {
     "revision": "71e3705946188729e691",
     "url": "/movie-app-project/static/js/1.71e37059.chunk.js"
   },
   {
-    "revision": "adc695b2d30ed970c531",
-    "url": "/movie-app-project/static/css/main.96fdc63c.chunk.css"
+    "revision": "2a06c553ef2318014dec",
+    "url": "/movie-app-project/static/css/main.01871fdb.chunk.css"
   },
   {
-    "revision": "45cf0687964974a2c384e650936d259d",
+    "revision": "36910ca8e0947cfa32348a0ae9184ddc",
     "url": "/movie-app-project/index.html"
   }
 ];
